02b62a885818e04e30b0148c5c91a37112e4e192
roshni-rawat-cs18 <roshni.rawat_cs18@gla.ac.in>
2021-12-11 05:28:29 +0530

    assignment complete

---
+++ Assignment_12/src/ts/spine/index.ts
@@ -0,0 +1,6 @@
+import { SpineParser } from 'pixi-spine';
+
+export { SpineParser };
+export * from '@pixi-spine/runtime-4.0';
+export * from '@pixi-spine/base';
+SpineParser.registerLoaderPlugin();
